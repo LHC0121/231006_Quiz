3f639b5800537c2e38e15c0a2e4651d1c9279da5
LHC0121 <144409044+LHC0121@users.noreply.github.com>
2023-10-09 12:25:45 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 number = int(input("몇 단까지 출력할까요?"))
-for x in range(1,number+1):
-    print("------- [" + str(x) + "단] -------")
-    for y in range(1,number+1):
-        print(x, "X", y, "=", x*y)
+def multiplicationtable(number):
+    for x in range(1,number+1):
+        print("------- [" + str(x) + "단] -------")
+        for y in range(1,number+1):
+            print(x, "X", y, "=", x*y)
+print(multiplicationtable(number))
 print("--------------------")
